5ce937ba3615687d79120fefab8df47aa6ea7487
idedalusse <31492548+idedalusse@users.noreply.github.com>
2017-11-02 15:43:48 -0400

    Initial Commit

---
+++ AddUp/AppDelegate.swift
@@ -0,0 +1,122 @@
+//
+//  AppDelegate.swift
+//  AddUp
+//
+//  Created by Mario Geneau on 2016-08-24.
+//  Copyright © 2016 Mario Geneau. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+    
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
+        sleep(2)// pour controller l'affichage le lunchScreen
+        return true
+    }
+    
+    
+    // MARK: - Types
+    
+    enum ShortcutIdentifier: String {
+        case First
+        case Second
+        case Third
+        case Fourth
+        
+        // MARK: - Initializers
+        
+        init?(fullType: String) {
+            guard let last = fullType.components(separatedBy: ".").last else {return nil}
+            
+            
+            self.init(rawValue: last)
+        }
+        
+        // MARK: - Properties
+        
+        var type: String {
+            return Bundle.main.bundleIdentifier! + ".\(self.rawValue)"
+        }
+    }
+    
+    // MARK: - Static Properties
+    
+    static let applicationShortcutUserInfoIconKey = "applicationShortcutUserInfoIconKey"
+
+    var window: UIWindow?
+
+    func applicationWillResignActive(_ application: UIApplication) {
+        // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
+        // Use this method to pause ongoing tasks, disable timers, and invalidate graphics rendering callbacks. Games should use this method to pause the game.
+    }
+
+    func applicationDidEnterBackground(_ application: UIApplication) {
+        // Use this method to release shared resources, save user data, invalidate timers, and store enough application state information to restore your application to its current state in case it is terminated later.
+        // If your application supports background execution, this method is called instead of applicationWillTerminate: when the user quits.
+    }
+
+    func applicationWillEnterForeground(_ application: UIApplication) {
+        // Called as part of the transition from the background to the active state; here you can undo many of the changes made on entering the background.
+    }
+
+    func applicationDidBecomeActive(_ application: UIApplication) {
+        // Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was previously in the background, optionally refresh the user interface.
+    }
+
+    func applicationWillTerminate(_ application: UIApplication) {
+        // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
+    }
+
+    func application(_ application: UIApplication, performActionFor shortcutItem: UIApplicationShortcutItem, completionHandler: @escaping (Bool) -> Void) {
+        let handledShortCutItem = handleShortCutItem(shortcutItem: shortcutItem)
+        
+        completionHandler(handledShortCutItem)
+    }
+    
+    func handleShortCutItem(shortcutItem: UIApplicationShortcutItem) -> Bool {
+        var handled = false
+        
+        // Verify that the provided `shortcutItem`'s `type` is one handled by the application.
+        guard ShortcutIdentifier(fullType: shortcutItem.type) != nil else { return false }
+        
+        guard let shortCutType = shortcutItem.type as String? else { return false }
+        
+        switch (shortCutType) {
+        case ShortcutIdentifier.First.type:
+            handled = true
+            break
+        case ShortcutIdentifier.Second.type:
+            // Handle shortcut 2 (static).
+            self.window?
+            .rootViewController!
+            .performSegue(withIdentifier: "mySeg", sender: nil)
+            handled = true
+            break
+        case ShortcutIdentifier.Third.type:
+            // Handle shortcut 3 (dynamic).
+            handled = true
+            break
+        case ShortcutIdentifier.Fourth.type:
+            // Handle shortcut 4 (dynamic).
+            handled = true
+            break
+        default:
+            break
+        }
+        
+        
+        return handled
+    }
+    
+    func application(application: UIApplication, performActionForShortcutItem shortcutItem: UIApplicationShortcutItem, completionHandler: (Bool) -> Void) {
+        
+        let handledShortCutItem = handleShortCutItem(shortcutItem: shortcutItem)
+        
+        completionHandler(handledShortCutItem)
+
+        
+    }
+}
+
